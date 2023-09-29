26260e9f2ac050eed49a200a5a8f358d790d53e9
G4J1R4 <115780716+G4J1R4@users.noreply.github.com>
2023-09-29 16:23:55 +0700

    Add files via upload

---
+++ WP2_Theodorus-Savta-V-N-master/Tugas 1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya adalah Theodorus Savta Viery N, Saya tinggal di Salatiga dan hobi saya mendengerkan musik";
+    }
+}
